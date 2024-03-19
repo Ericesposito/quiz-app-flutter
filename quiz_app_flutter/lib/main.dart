ff8e68dbbda2beb45a154f1b9f87e3ca71c84d64
Ericesposito <eespo93@optonline.net>
2024-03-19 19:00:33 -0400

    successfully lifted up state between Widgets and invoked functions across widget lines

--- quiz_app_flutter/lib/main.dart
+++ quiz_app_flutter/lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 
-// import 'package:quiz_app_flutter/gradient_container.dart';
-import 'package:quiz_app_flutter/start_screen.dart';
+import 'package:quiz_app_flutter/quiz.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: StartScreen(),
-      ),
-    ),
+    const Quiz(),
   );
 }
---
+++ quiz_app_flutter/lib/quiz.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'package:quiz_app_flutter/start_screen.dart';
+import 'package:quiz_app_flutter/questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(
+      () {
+        activeScreen = const QuestionsScreen();
+        print('Changing state');
+      },
+    );
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 55, 6, 141),
+                Color.fromARGB(255, 104, 54, 189),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
